29d4a72ac37555a878c2c263e837f8a0ad477563
Mohd Zain <mohammedzain4099@gmail.com>
2024-07-11 16:30:13 +0530

    update functionalites

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -12,12 +12,19 @@ function App() {
     setTodoList((prev) => [...prev, todo]);
   };
 
+  const handleDeleteTodo = (id) => {
+    setTodoList(() => todoList.filter((todo) => todo.id !== id));
+  };
+
   return (
     <center>
       <div className="container">
         <AppName></AppName>
         <TodoInput handleAddTodo={handleAddTodo}></TodoInput>
-        <TodoItems todoList={todoList}></TodoItems>
+        <TodoItems
+          todoList={todoList}
+          handleDeleteTodo={handleDeleteTodo}
+        ></TodoItems>
       </div>
     </center>
   );
--- TodoApp/src/Components/TodoItems.jsx
+++ TodoApp/src/Components/TodoItems.jsx
@@ -1,11 +1,11 @@
-const TodoItems = ({ todoList }) => {
+const TodoItems = ({ todoList, handleDeleteTodo }) => {
   return (
     <div>
           {todoList.map((todo) => (
         <div className="todoItemsContainer" key={todo.id}>
           <div className="todoLabel">{todo.todoLabel}</div>
           <div className="dueDate">{todo.dueDate}</div>
-          <button className="deleteButton">delete</button>
+          <button className="deleteButton" onClick={() => handleDeleteTodo(todo.id)}>delete</button>
         </div>
       ))}
     </div>
