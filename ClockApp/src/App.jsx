c3af2b861f5729164246267d71a4c235ba76d9ac
Mohd Zain <mohammedzain4099@gmail.com>
2024-07-20 14:38:03 +0530

    update clock app

--- ClockApp/src/App.jsx
+++ ClockApp/src/App.jsx
@@ -5,15 +5,16 @@ import TimeSection from "./Components/TimeSection";
 import DateSection from "./Components/DateSection";
 
 function App() {
-  
   return (
     <ClockContextProvider>
       <div className="container">
         <h1 className="clock-heading">Digital Clock</h1>
         <div className="card">
-          <TimeFormat/>
-          <TimeSection/>
-          <DateSection/>
+          <TimeFormat />
+          <div className="mid-section">
+            <TimeSection />
+            <DateSection />
+          </div>
         </div>
       </div>
     </ClockContextProvider>
